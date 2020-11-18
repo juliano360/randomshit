f6ea06d15beac4c47a2c2a334e1d8f82403f9a90
juliano360 <66537152+juliano360@users.noreply.github.com>
2020-11-18 12:54:42 -0300

    Add files via upload

---
+++ x.py
@@ -0,0 +1,10 @@
+# Tu programa
+if(button != ''): sql(button) #Si el botón está vacío no se va a ejecutar nada
+# Tu programa
+
+
+def call_sql(b):
+    while(True):
+        db.execute("whatevergoeshere... {0}.jpg ... whateverhastalascomillas".format(b))
+        # El resto del código este que tenés la llamada a SQL
+        break
